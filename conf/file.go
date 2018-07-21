09d7322827ccf78713c40a3f572b419c1a530289
Sergey Seryiza Zaborovsky <seryiza@yandex.ru>
2018-07-21 14:48:12 +1100

    Добавлена реализация conf/file.go (только для получения из файла)

--- conf/file.go
+++ conf/file.go
@@ -1,1 +1,33 @@
 package conf
+
+import (
+	"encoding/json"
+	"os"
+
+	"golang.org/x/oauth2"
+)
+
+const (
+	ConfigFilePostfix = "_CONF_FILE"
+)
+
+func FromFile(envPrefix string) (*oauth2.Config, error) {
+	filename := getFilename(envPrefix)
+	configFile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	conf := &oauth2.Config{}
+	jd := json.NewDecoder(configFile)
+	if err = jd.Decode(conf); err != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}
+
+func getFilename(prefix string) string {
+	return os.Getenv(prefix + ConfigFilePostfix)
+}
